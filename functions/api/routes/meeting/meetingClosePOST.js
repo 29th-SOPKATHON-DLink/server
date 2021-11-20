fe30d2f44b4c6039685b5feb15c6d0b1330254f5
sssua-0928 <hansua990928@gmail.com>
2021-11-21 07:14:49 +0900

    미팅 종료 수정

--- functions/api/routes/meeting/meetingClosePOST.js
+++ functions/api/routes/meeting/meetingClosePOST.js
@@ -3,25 +3,36 @@ const util = require('../../../lib/util');
 const statusCode = require('../../../constants/statusCode');
 const responseMessage = require('../../../constants/responseMessage');
 const db = require('../../../db/db');
-const { meetingDB } = require('../../../db');
+const {
+  meetingDB
+} = require('../../../db');
 
 
 module.exports = async (req, res) => {
-const {id} = req.params;
-const {image, soju, beer, makgeolli, etc, review} = req.body;
+  const {
+    id
+  } = req.params;
+  const {
+    image,
+    soju,
+    beer,
+    makgeolli,
+    etc,
+    review
+  } = req.body;
 
-// 에러 트래킹을 위해 try / catch문을 사용합니다.
-// try문 안에서 우리의 로직을 실행합니다.
-let client;
+  // 에러 트래킹을 위해 try / catch문을 사용합니다.
+  // try문 안에서 우리의 로직을 실행합니다.
+  let client;
 
   try {
     client = await db.connect(req);
-    const postcheck = await meetingDB.checkPost(client, id);
-    const post = await meetingDB.addPost(client, id, image, soju, beer, makgeolli, etc, review);
 
-    
-   
-    
+    const post = await meetingDB.addPost(client, id, image, soju, beer, makgeolli, etc, review);
+    const postcheck = await meetingDB.checkPost(client, id);
+
+
+
     res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.ADD_ONE_POST_SUCCESS, post));
   } catch (error) {
     functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
