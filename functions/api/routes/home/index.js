8b095307b87b82e5592fc2291a95c933ddbc9ffa
sssua-0928 <hansua990928@gmail.com>
2021-11-21 00:08:47 +0900

    routes 설정

--- functions/api/routes/home/index.js
+++ functions/api/routes/home/index.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const router = express.Router();
 
-router.use('/meeting', require('./meeting'));
-router.use('/home', require('./home'));
 
 module.exports = router;
